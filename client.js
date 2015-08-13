81f2792656596673e3ebaf2c5215f7c51c5c38c9
kailocks <quan.hungbang@gmail.com>
2015-08-13 22:17:23 +0700

    create chat app

--- client.js
+++ client.js
@@ -1,6 +1,13 @@
-$(document).ready(function() {
+$(document).ready(function() {	
 	var socket = io.connect('http://127.0.0.1:8088');
 	socket.on('text', function(data) {
-		alert(data);
+		console.log(data);
 	});
+
+
 });
+var socket = io.connect('http://127.0.0.1:8088');
+function fnSubmit(ev){
+	var user = document.getElementById('user').value;
+	socket.emit('new-user', user);
+}
--- server.js
+++ server.js
@@ -1,14 +1,31 @@
+// var server = require('http').createServer();
+// var io = require('socket.io')(server);
+
+// io.sockets.on('connection', function (socket) {
+//     console.log('socket connected');
+
+//     socket.on('disconnect', function () {
+//         console.log('socket disconnected');
+//     });
+//     socket.emit('text', 'wow. such event. very real time.');
+// });
+
+// server.listen(8088);
+
 var server = require('http').createServer();
 var io = require('socket.io')(server);
 
-io.sockets.on('connection', function (socket) {
-    console.log('socket connected');
+io.sockets.on('connection', function(socket){
+	console.log('socket is connected');
+	socket.on('disconnect', function(){
+		console.log('socket is disconnected');
+	});
 
-    socket.on('disconnect', function () {
-        console.log('socket disconnected');
-    });
-    socket.emit('text', 'wow. such event. very real time.');
+	socket.emit('text', 'hello the world');
+
+	socket.on('new-user', function(data){
+		console.log('User %s have joined', data);
+	});
 });
-
+io.set('origins', '*:*');
 server.listen(8088);
-
